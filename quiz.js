0a1f103781164fa20dd16f3b593fb4b19c045a92
Kwame Asah <emmanuelasah1000@gmail.com>
2024-10-04 14:56:45 +0000

    Add initial project files

---
+++ quiz.js
@@ -0,0 +1,22 @@
+
+
+const feedBack = document.getElementById('feedback');
+const submit = document.getElementById('submit-answer');
+
+
+// function that checks if the selected option is the right answer
+
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    // Get selected answer
+    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value
+    if (userAnswer === correctAnswer) {
+        feedBack.textContent = "Correct! Well done"
+    } else {
+        feedBack.textContent = "That's incorrect. Try again"
+    }
+}
+
+
+submit.addEventListener("click", checkAnswer);
